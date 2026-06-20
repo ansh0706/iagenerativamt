3c161ca820f7f2d4c1298844b40e3be33acea76a
ansh0706 <rafaelc151@hotmail.com>
2026-06-20 11:41:04 -0300

    Update PROJETO-FINAL.py

--- PROJETO-FINAL.py
+++ PROJETO-FINAL.py
@@ -35,7 +35,7 @@ if df.empty:
 # =====================================================================
 # 4. CONSTRUINDO O VISUAL DO SITE E OS GRÁFICOS
 # =====================================================================
-st.image("logo-senai-1.png")
+st.image("logo.png")
 st.title("🏭 Centro de Controle de Produção Avançado - IA Transformers")
 st.markdown("Monitoramento detalhado por **Setor** e **Operador**.")
 st.divider() 
